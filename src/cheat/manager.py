eb10f74f6c4baacc5ff0598214f5faf582184726
Miinh Nguyen <ducminh3112@gmail.com>
2023-04-16 13:23:57 +0700

    change repo name

--- src/cheat/manager.py
+++ src/cheat/manager.py
@@ -5,9 +5,9 @@ def main():
     ret = misc.get_choice(
         question="Select your work",
         options=[
-            {"desc": "Download new cheatcode", "selector":1}
-        ],
-        answer="Choice"
+            {"desc": "Update Title ID", "selector":1, "return":"update-title-id"},
+            {"desc": "Update cheatcode database", "selector":2, "return":"update-cheatcode-db"}
+        ]
     )
 
     print(ret, type(ret))
